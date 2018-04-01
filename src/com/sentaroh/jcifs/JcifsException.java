d4445cdc28417420cd47ab282ea1b704004efadb
sentaroh <gm.developer.fhoshino@gmail.com>
2018-04-01 20:20:25 +0900

    コード整理

--- src/com/sentaroh/jcifs/JcifsException.java
+++ src/com/sentaroh/jcifs/JcifsException.java
@@ -2,13 +2,20 @@ package com.sentaroh.jcifs;
 
 
 public class JcifsException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5966004473207908418L;
 	private Throwable mException=null;
-    private Throwable mCause=null;
+//    private Throwable mCause=null;
 	private int mNtStatus=0;
 	private String mMessage=null;
+//	private String mCause=null;
 	public JcifsException(Throwable e, int nt_status, Throwable cause ) {
-		mException=e;
-		mCause=cause;
+		mException=new Exception(e.getMessage(), cause);
+		mException.setStackTrace(e.getStackTrace());
+		
+//		mCause=cause.getMessage();
 		mNtStatus=nt_status;
         mMessage=e.getMessage();
 	}
@@ -17,7 +24,7 @@ public class JcifsException extends Exception {
 	    return mNtStatus;
     }
     public Throwable getCause() {
-        return mCause;
+        return mException.getCause();
     }
     public String getMessage() {
         return mMessage;
